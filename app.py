d1a0bb0333d13a11f951852ce3af075007eac888
Mr-houngbo <houngbo.calixte.r@gmail.com>
2026-06-02 10:06:57 +0000

    Mon premier code Streamlit

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Configuration de la page
+st.set_page_config(page_title="UEMOA Macro Pulse", layout="wide")
+
+# Notre premier titre
+st.title("📊 UEMOA Macro-Pulse Dashboard")
+st.write("Bienvenue dans ton terminal Bloomberg d'Afrique de l'Ouest.")
+st.info("Statut : En cours de développement...")
+
